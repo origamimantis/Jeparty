ebaf3e7a3d8509fe415d29bac5b0443cf2b6c53a
eric zhang <ezhang323@gmail.com>
2019-03-01 14:25:44 -0800

    implemented add/remove player, completion checks, scores

--- game.js
+++ game.js
@@ -18,52 +18,160 @@ let fs = require('fs')
 //   >>"answer 1"
 //   ...
 
+class Player {
+	constructor(username) {
+		this.name = username
+		this.score = 0
+	}
+	changeScore(amount){
+		this.score += amount
+	}
+}
 
 class SingleQuestion {
 	constructor(question, answer){
 		this.question = question
 		this.answer = answer
+		this.done = false
+	}
+	markComplete() {
+		this.done = true
 	}
 }
 
 
-let setList= ()=> {
-  	let hello = 'hello.txt';
-  	let readed = fs.readFileSync(hello, 'utf8');
-  	let rl = readed.split('||')
-	let dict = {}
-	for (chunk of rl) {
-		let categoryName = chunk.split('--')[0].trim()
-		let questions = chunk.split('--')[1]
-		qSplit = questions.split('>>')
-		subdict = {}
-		for (let ind = 1; ind < qSplit.length; ind ++) {
-			let question = qSplit[ind].split('>')[0].trim()
-			let answer = qSplit[ind].split('>')[1].trim()
 
-			subdict[(ind)*100] = new SingleQuestion( question, answer )
+
+class main  {
+	constructor(file) {
+		let readed = fs.readFileSync(file, 'utf8');
+		let rl = readed.split('\n||\n')
+		let dict = {}
+		for (let chunk of rl) {
+			let categoryName = chunk.split('\n--')[0].trim()
+			let questions = chunk.split('\n--')[1]
+			let qSplit = questions.split('\n>>')
+			let subdict = {}
+			for (let ind = 1; ind < qSplit.length; ind ++) {
+				let question = qSplit[ind].split('\n>')[0].trim()
+				let answer = qSplit[ind].split('\n>')[1].trim()
+
+				subdict[(ind)*100] = new SingleQuestion( question, answer )
+			}
+			dict[categoryName] = subdict
 		}
-		dict[categoryName] = subdict
-
-
+		
+		this.state = dict
+		this.players = []
+		this.names = []
 	}
-	console.log(dict)
-  
+	addPlayer(name) {
+		if (!(this.names.includes(name))) {
+			this.players.push(new Player(name))
+			this.names.push(name)
+		}
+		
+		else {
+			console.log('player already exists.')
+		}
+	}
+
+	rmvPlayer(name) {
+		let idx = this.names.indexOf(name)
+		if (idx != -1){
+			this.players.splice(idx,1)
+			this.names.splice(idx,1)
+		}
+		else {
+			console.log('this player doesn\'nt exist.')
+		}
+	}
+
+
+	modifyScore(name, amount) {
+
+		this.players[this.names.indexOf(name)].changeScore(amount)
+	}
+
+	answerQuestion(name, category, amount, correct) {
+		if (this.state[category][amount].done) {
+			console.log('this question was answered already.')
+		}
+		else if (correct) {
+			this.modifyScore(name, amount)
+			this.state[category][amount].markComplete()
+		}
+		else {
+			this.modifyScore(name, -1*amount)
+		}
+	}
+
 
 }
-  
-setList()
+
+	
+
+let h =new main('hello.txt')
+
+console.log(h.state)
+console.log(h.players)
+
+console.log('\neric joins\n')
+
+h.addPlayer('eric')
+console.log(h.players)
+
+console.log('\nbob is kicked out\n')
+
+h.rmvPlayer('bob')
+console.log(h.players)
 
 
+console.log('\neric joins\n')
 
+h.addPlayer('eric')
+console.log(h.players)
 
+console.log('\nmax joins\n')
 
-class Game {
-  constructor( file ) {
-  }
-}
+h.addPlayer('max')
+console.log(h.players)
 
+console.log('\narcq joins\n')
 
+h.addPlayer('arcq')
+console.log(h.players)
 
+console.log('\narcq is kicked out\n')
 
-let h = []
+h.rmvPlayer('arcq')
+console.log(h.players)
+
+console.log('\neric gets 100 points\n')
+
+h.modifyScore('eric', 100)
+console.log(h.players)
+
+console.log('\neric correctly answers People 200\n')
+
+h.answerQuestion('eric','People',200,true)
+console.log(h.state)
+console.log(h.players)
+
+console.log('\nmax incorrectly answers IDEs 100\n')
+
+h.answerQuestion('max','IDEs',100,false)
+console.log(h.state)
+console.log(h.players)
+
+console.log('\neric correctly answers IDEs 100\n')
+
+h.answerQuestion('eric','IDEs',100,true)
+console.log(h.state)
+console.log(h.players)
+
+console.log('\nmax correctly answers IDEs 100\n')
+
+h.answerQuestion('max','IDEs',100,true)
+console.log(h.state)
+console.log(h.players)
