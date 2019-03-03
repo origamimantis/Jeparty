b1f0e6baab336270551b482845dda78834ef8595
eric zhang <ezhang323@gmail.com>
2019-03-02 21:47:24 -0800

    updated to listen on 3 separate ports,as well as write to viewer

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ const net= require('net');
 const host = 'localhost';
 const playerport = 8016;
 const masterport = 8418;
+const viewerport = 8791;
 
 
 class Player {
@@ -73,6 +74,7 @@ class main  {
 			dict[categoryName] = subdict}
 
 		this.currentQuestion = null;
+		this.currentCategory = null
 		this.currentPlayer = null;
 		this.state = dict}
 
@@ -80,8 +82,9 @@ class main  {
 		players[this.currentPlayer].score += amount}
 
 	markCurrentQuestionComplete() {
-		this.currentQuestion.markComplete()
-		this.currentQuestion = null}
+		this.currentQuestion.markComplete();
+		this.currentQuestion = null;
+		this.currentCategory = null}
 }
 
 	
@@ -105,8 +108,11 @@ let playerID = 0;
 
 let master = {}
 
-// Used to probably signal when buzzing is allowed
-function writetoPlayers() {}
+function writeToViewers(data) {
+	for (let v of viewers) {
+		v.write(data + '\r\n');
+	}
+}
 
 
 function enableAnswers() {
@@ -121,7 +127,7 @@ function enableAnswers() {
 
 const playerserver = net.createServer( (player) => {
 	
-	player.added = false
+	player.added = false;
 
 	player.on('data', (data) => {
 
@@ -133,112 +139,156 @@ const playerserver = net.createServer( (player) => {
 			if (!player.added && (data.toString().search('PLAYER') == 0)) {
 				
 				// create a unique id by adding 1
-				player.id = playerID ++
+				player.id = playerID ++;
 
 				// set id:Player{name:name, score:0}
-				players[player.id] = new Player(data.toString().trim().split(' ')[1])
+				players[player.id] = new Player(data.toString().trim().split(' ')[1]);
 				
-				player.added = true
+				player.added = true;
+
+				writeToViewers( players[player.id].name + ' (id: ' + player.id + ') has joined the game!');
 			}
 		}
 
 		else if (gameStatus == 'playing' && players[player.id].canAnswer) {
-			players[player.id].canAnswer = false
-			gameStatus = 'reviewing'
-			theGame.currentPlayer = player.id
+			players[player.id].canAnswer = false;
+			gameStatus = 'reviewing';
+			theGame.currentPlayer = player.id;
+			
+			writeToViewers( players[player.id].name + ' (id: ' + player.id + ') buzzed in!');
+			writeToViewers( 'Waiting for game host to determine if '+ players[player.id].name + ' is correct.');
 		}
 	});
 	player.on('close', () => {
 		// maybe not delete if we want score to be kept.
-		delete players[player.id]
+		writeToViewers( players[player.id].name + ' (id: ' + player.id + ') has left.');
+		delete players[player.id];
 	});
 });
 	
 const masterserver = net.createServer( (socket) => {
 
 	socket.on('data', (data) => {
-		let received = data.toString().trim()
-		console.log(received)
-		if (received == 'stat') {
-			console.log(theGame.state)
+		let received = data.toString().trim();
+		if (received == 'STATS') {
+			console.log(theGame.state);
 		}
 		if (gameStatus == 'waiting') {
 			
 			// checks if master does not yet exist
 			if(!master.name && received.search('MASTER') == 0 && received.split(' ').length == 3){
-				master.name  = received.split(' ')[1]
-				master.id  = 'M' + received.split(' ')[2]
-				console.log(master)
+				master.name  = received.split(' ')[1];
+				master.id  = 'M' + received.split(' ')[2];
+				
+				writeToViewers( 'Game host ' + master.name + ' (id: ' + master.id + ') has appeared!');
 			}
 			else if (received == 'GAME_BEGIN') {
-				console.log('began')
-				gameStatus = 'selecting'
+				gameStatus = 'selecting';
+				
+				writeToViewers( 'Game host '+master.name+' (id: '+master.id+') is selecting a question.');
 			}
 			else if (master.name && received.search('MASTER') == 0){
-				socket.destroy()
+				socket.destroy();
+				
+				writeToViewers( received.split(' ')[1] + ' attempted to join as the game host.');
 			}
 		}
 		else if (gameStatus == 'selecting') {
 			// form 'SELECT category amount'
 			if (received.search('SELECT') == 0 && received.split(' ').length == 3) {
 				//current question is now a class instance
-				theGame.currentQuestion = theGame.state[received.split(' ')[1]][received.split(' ')[2]]
-				gameStatus = 'playing'
+				theGame.currentQuestion = theGame.state[received.split(' ')[1]][received.split(' ')[2]];
+				theGame.currentCategory = received.split(' ')[1];
+				gameStatus = 'playing';
+				
+				writeToViewers( 'The category is: '+received.split(' ')[1]+' for '+received.split(' ')[2] );
+				writeToViewers( '---> ' + theGame.currentQuestion.question )
 			}
 		}
 		else if (gameStatus == 'playing') {
 			if (received == 'QUESTION_SKIP') {
 				//mark question complete, proceed to selecting
-				theGame.markCurrentQuestionComplete()
-				enableAnswers()
-				gameStatus = 'selecting'
+				theGame.markCurrentQuestionComplete();
+				enableAnswers();
+				gameStatus = 'selecting';
 			}
 		}
 		else if (gameStatus == 'reviewing') {
 			if (received == 'QUESTION_SKIP') {
 				//mark complete, proceed to selecting
-				theGame.markCurrentQuestionComplete()
-				enableAnswers()
-				gameStatus = 'selecting'
+				theGame.markCurrentQuestionComplete();
+				enableAnswers();
+				gameStatus = 'selecting';
+				
+				writeToViewers( 'Game host '+master.name+' (id: '+master.id+') skipped the current question.');
+				writeToViewers( 'Game host '+master.name+' (id: '+master.id+') is selecting a question.');
 			}
 			else if (received == 'QUESTION_CORRECT') {
 				//mark complete, add points, proceed to selecting
-				theGame.modifyScore(theGame.currentQuestion.amount)
-				theGame.markCurrentQuestionComplete()
-				enableAnswers()
-				gameStatus = 'selecting'
+				theGame.modifyScore(theGame.currentQuestion.amount);
+				enableAnswers();
+				gameStatus = 'selecting';
+				
+				writeToViewers( players[theGame.currentPlayer].name+' (id: ' + theGame.currentPlayer + ') answered correctly!');
+				writeToViewers( 'The answer was: ' + theGame.currentQuestion.answer )
+				writeToViewers( players[theGame.currentPlayer].name + ' received ' + theGame.currentQuestion.amount + ' points.');
+				
+				theGame.markCurrentQuestionComplete();
+				
+				writeToViewers( 'Game host '+master.name+' (id: '+master.id+') is selecting a question.');
 			}
 			else if (received == 'QUESTION_INCORRECT') {
 				//subtract points, proceed to playing
-				theGame.modifyScore(-1*theGame.currentQuestion.amount)
-				gameStatus = 'playing'
+				theGame.modifyScore(-1*theGame.currentQuestion.amount);
+				gameStatus = 'playing';
+				
+				writeToViewers( players[theGame.currentPlayer].name+' (id: ' + theGame.currentPlayer + ') answered incorrectly!');
+				writeToViewers( players[theGame.currentPlayer].name + ' lost ' + theGame.currentQuestion.amount + ' points.');
+				
+				writeToViewers( 'The category is: '+theGame.currentCategory+' for '+theGame.currentQuestion.amount );
+				writeToViewers( '---> ' + theGame.currentQuestion.question )
+				
 			}
 		}
 				
 
 	});  //end on 'data'
 	socket.on('close', () => {
-		delete master.name
-		delete master.id
+		delete master.name;
+		delete master.id;
 	});  //end on 'close'
 				
 	
 });
 
 
+let viewers = []
+
+const viewerserver = net.createServer( (viewer) => {
+	viewer.setNoDelay();
+	viewers.push(viewer);
+	viewer.write('Welcome to Jeparty!\r\n')
+	viewer.on('close', () => {});
+})
+
+
+
+
+
 playerserver.listen(playerport, host, () => {})
 masterserver.listen(masterport, host, () => {})
+viewerserver.listen(viewerport, host, () => {})
 
 
 
 // log updates to console
-setInterval(() => {
-	console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
-	console.log('-------\n')
-	console.log(players)
-	console.log(master)
-	console.log(gameStatus)
-	console.log(theGame.currentQuestion)
-	console.log('\n')
-}, 3000);
+//setInterval(() => {
+//	console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+//	console.log('-------\n')
+//	console.log(players)
+//	console.log(master)
+//	console.log(gameStatus)
+//	console.log(theGame.currentQuestion)
+//	console.log('\n')
+//}, 3000);
 
